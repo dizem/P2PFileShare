93ee5f7f3339def5679942a974dd3d43244198c4
dizem <dizem@126.com>
2015-01-07 01:18:27 +0800

    modify code style

--- ChunkFile.java
+++ ChunkFile.java
@@ -3,46 +3,46 @@
  */
 public class ChunkFile {
 
-    // Name of the file
-    private final String filename;
+  // Name of the file
+  private final String filename;
 
-    // Size of the file
-    private final long fileSize;
+  // Size of the file
+  private final long fileSize;
 
-    // Number of chunks
-    private final int chunkNum;
+  // Number of chunks
+  private final int chunkNum;
 
-    /**
-     * Constructor
-     *
-     * @param chunkNum Number of chunks
-     * @param fileSize Size of the file
-     * @param filename Name of the file
-     */
-    public ChunkFile(int chunkNum, long fileSize, String filename) {
-        this.chunkNum = chunkNum;
-        this.filename = filename;
-        this.fileSize = fileSize;
-    }
+  /**
+   * Constructor
+   *
+   * @param chunkNum Number of chunks
+   * @param fileSize Size of the file
+   * @param filename Name of the file
+   */
+  public ChunkFile(int chunkNum, long fileSize, String filename) {
+    this.chunkNum = chunkNum;
+    this.filename = filename;
+    this.fileSize = fileSize;
+  }
 
-    /**
-     * @return Name of the file
-     */
-    public String getFilename() {
-        return filename;
-    }
+  /**
+   * @return Name of the file
+   */
+  public String getFilename() {
+    return filename;
+  }
 
-    /**
-     * @return Size of the file
-     */
-    public long getFileSize() {
-        return fileSize;
-    }
+  /**
+   * @return Size of the file
+   */
+  public long getFileSize() {
+    return fileSize;
+  }
 
-    /**
-     * @return Number of chunks
-     */
-    public int getChunkNum() {
-        return chunkNum;
-    }
+  /**
+   * @return Number of chunks
+   */
+  public int getChunkNum() {
+    return chunkNum;
+  }
 }
--- Server.java
+++ Server.java
@@ -11,90 +11,90 @@ import java.util.Scanner;
  */
 public class Server {
 
-    // Config of the program
-    private final Properties config;
+  // Config of the program
+  private final Properties config;
 
-    // File to be distributed
-    private final ChunkFile file;
+  // File to be distributed
+  private final ChunkFile file;
 
-    // Server connection
-    private final ServerSocket server;
+  // Server connection
+  private final ServerSocket server;
 
-    // Number connected clients
-    private int connectedClients;
+  // Number connected clients
+  private int connectedClients;
 
-    /**
-     * Constructor
-     *
-     * @param config   Config of the program
-     * @param filename Name of the file
-     * @param fileSize Size of the file
-     * @param chunkNum Number of chunks
-     */
-    public Server(Properties config, String filename, long fileSize, int chunkNum)
-            throws IOException {
+  /**
+   * Constructor
+   *
+   * @param config   Config of the program
+   * @param filename Name of the file
+   * @param fileSize Size of the file
+   * @param chunkNum Number of chunks
+   */
+  public Server(Properties config, String filename, long fileSize, int chunkNum)
+      throws IOException {
 
-        this.config = config;
-        this.file = new ChunkFile(chunkNum, fileSize, filename);
-        this.connectedClients = 0;
+    this.config = config;
+    this.file = new ChunkFile(chunkNum, fileSize, filename);
+    this.connectedClients = 0;
 
-        // Get server port from the config
-        int port = Integer.parseInt(config.getProperty("ServerPort"));
-        // Start the server socket
-        this.server = new ServerSocket(port);
+    // Get server port from the config
+    int port = Integer.parseInt(config.getProperty("ServerPort"));
+    // Start the server socket
+    this.server = new ServerSocket(port);
 
-        // Listen to the clients
-        startListening();
+    // Listen to the clients
+    startListening();
+  }
+
+  public static void main(String[] args) {
+    System.out.println("The file is running.");
+    Scanner input = new Scanner(System.in);
+    try {
+      // Load config
+      Properties config = new Properties();
+      config.load(new FileInputStream("config.properties"));
+
+      int chunkSize = Integer.parseInt(config.getProperty("ChunkSize"));
+      String chunkDir = config.getProperty("ChunkDir");
+
+      // Enter the filename
+      System.out.println("Enter the filename:");
+      String filename = input.next();
+
+      // Split the file into chunks
+      File file = new File(filename);
+      int n = SplitFiles.split(chunkSize, chunkDir, file);
+
+      // Start the server
+      new Server(config, file.getName(), file.length(), n);
+
+    } catch (IOException e) {
+      System.out.println("Error: " + e.getMessage());
     }
+  }
 
-    /**
-     * Listens to the clients
-     */
-    public void startListening() {
+  /**
+   * Listens to the clients
+   */
+  public void startListening() {
 
-        // Get the total number of clients
-        int clients = Integer.parseInt(config.getProperty("ClientNumber"));
+    // Get the total number of clients
+    int clients = Integer.parseInt(config.getProperty("ClientNumber"));
 
-        // If all the clients connected, exit the loop
-        while (connectedClients < clients) {
-            System.out.println("Waiting for connection...");
+    // If all the clients connected, exit the loop
+    while (connectedClients < clients) {
+      System.out.println("Waiting for connection...");
 
-            try {
-                // If a client is connected, start a handle thread
-                Socket socket = server.accept();
-                new ServerHandler(config, file, socket).start();
-                connectedClients++;
+      try {
+        // If a client is connected, start a handle thread
+        Socket socket = server.accept();
+        new ServerHandler(config, file, socket).start();
+        connectedClients++;
 
-            } catch (IOException e) {
-                System.out.println("Error: " + e.getMessage());
-            }
-        }
+      } catch (IOException e) {
+        System.out.println("Error: " + e.getMessage());
+      }
     }
-
-    public static void main(String[] args) {
-        System.out.println("The file is running.");
-        Scanner input = new Scanner(System.in);
-        try {
-            // Load config
-            Properties config = new Properties();
-            config.load(new FileInputStream("config.properties"));
-
-            int chunkSize = Integer.parseInt(config.getProperty("ChunkSize"));
-            String chunkDir = config.getProperty("ChunkDir");
-
-            // Enter the filename
-            System.out.println("Enter the filename:");
-            String filename = input.next();
-
-            // Split the file into chunks
-            File file = new File(filename);
-            int n = SplitFiles.split(chunkSize, chunkDir, file);
-
-            // Start the server
-            new Server(config, file.getName(), file.length(), n);
-
-        } catch (IOException e) {
-            System.out.println("Error: " + e.getMessage());
-        }
-    }
+  }
 }
